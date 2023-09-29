5c7c5b3add7c1fb0b387677a5e25e787ae487799
Kays8 <100181421+siwakorn22@users.noreply.github.com>
2023-09-29 11:32:24 +0700

    birthday-list

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import data from './data'
+import List from './List';
+
+function App() {
+  const [people, setPeople] = useState(data);
+  const clearAll = () =>{
+    setPeople([]);
+  }
+  return (
+    <div>
+      <h1>Workshop 1 : Birthday List Project</h1>
+      <main>
+        <section className="container">
+          <h3>{people.length} birthdays today</h3>
+          <List people={people} />
+          <button onClick={clearAll}>Clear All</button>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App
